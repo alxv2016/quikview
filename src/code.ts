2736f64eb19ffc2ed0eeed7aff7358139bbb92d6
Alex Vong <avong2011@icloud.com>
2024-06-22 14:15:11 -0400

    authoring fix

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,6 @@
 figma.showUI(__html__, {height: 575, width: 430, title: 'A Figma plugin', themeColors: true});
 
+//Figma api service
 figma.ui.onmessage = (message) => {
   console.log('CODE LOG', message);
   // sending a message back to the ui in a half second...
