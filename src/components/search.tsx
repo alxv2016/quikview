c041ca2a268199a5e7bca7fe25783d4653c2411f
Alex Vong <avong2011@icloud.com>
2024-06-25 22:55:21 -0400

    fix: active index returning NaN when no results or empty array

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,7 +1,8 @@
-import {useState, useEffect, useRef, useMemo} from 'react';
+import {Fragment, useState, useEffect, useRef, useMemo} from 'react';
 import Fuse from 'fuse.js';
 import Input from '../components/input';
 import {Guideline, Critiera, Successcriterion} from '../data/wcag.interface';
+import './search.scss';
 
 interface SearchProps {
   data: Successcriterion[];
@@ -9,7 +10,10 @@ interface SearchProps {
 }
 function Search({data, keys}: SearchProps): JSX.Element {
   const [query, setQuery] = useState('');
+  const [debouncedQuery, setDebouncedQuery] = useState(query);
   const [results, setResults] = useState<Successcriterion[] | null>(null);
+  const [announcement, setAnnouncement] = useState('');
+
   const [isFocused, setIsFocused] = useState(false);
   const [activeIndex, setActiveIndex] = useState(-1);
   const searchInputRef = useRef<HTMLInputElement>(null);
@@ -25,53 +29,105 @@ function Search({data, keys}: SearchProps): JSX.Element {
     [data, keys]
   );
 
-  console.log('fuse', results);
+  console.log('index', activeIndex);
 
   useEffect(() => {
-    if (query) {
-      const result = fuse.search(query).map(({item}) => item);
+    const handler = setTimeout(() => {
+      if (results) {
+        setAnnouncement(
+          results.length === 0 ? 'No results found' : `${results.length} result${results.length > 1 ? 's' : ''} found`
+        );
+      }
+    }, 300); // Debounce period
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [results]);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, 100);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [query]);
+
+  useEffect(() => {
+    if (debouncedQuery) {
+      const result = fuse.search(debouncedQuery).map(({item}) => item);
       setResults(result);
     } else {
       setResults(null);
     }
-  }, [query, fuse]);
+  }, [debouncedQuery, fuse]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
+    setActiveIndex(-1);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!results) return;
+
     switch (e.key) {
       case 'ArrowDown':
-        if (results) {
-          setActiveIndex((prevIndex) => (prevIndex < results.length - 1 ? prevIndex + 1 : prevIndex));
+        e.preventDefault();
+        // Move the selection to the next item in the list
+        // (prevIndex + 1) % results.length ensures that after the last item, it wraps back to the first item
+        if (results && results.length > 0) {
+          setActiveIndex((prevIndex) => (prevIndex + 1) % results.length);
         }
         break;
       case 'ArrowUp':
-        setActiveIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 0));
+        e.preventDefault();
+        // Move the selection to the previous item in the list
+        // (prevIndex - 1 + results.length) % results.length ensures that before the first item, it wraps back to the last item
+        if (results && results.length > 0) {
+          setActiveIndex((prevIndex) => (prevIndex - 1 + results.length) % results.length);
+        }
         break;
       case 'Enter':
         if (activeIndex >= 0) {
-          if (results) {
-            setQuery(results[activeIndex].title);
+          e.preventDefault();
+          setQuery(results[activeIndex].title);
+          setActiveIndex(-1);
+          // NOTE: should find a way to clear results and prevent Fuse to run another search
+          // on selected keyboards, this doesn't prevent that and Fuse will still run a search to just be cleared.
+          setTimeout(() => {
             setResults(null);
-          }
+          }, 200);
+        }
+        break;
+      case ' ':
+        if (activeIndex >= 0) {
+          e.preventDefault();
+          setQuery(results[activeIndex].title);
+          setActiveIndex(-1);
+          setTimeout(() => {
+            setResults(null);
+          }, 200);
         }
         break;
       case 'Escape':
         setResults(null);
         break;
+
       default:
         break;
     }
   };
 
-  const handleResultClick = (result: Critiera) => {
+  const handleResultClick = (result: Successcriterion) => {
     setQuery(result.title);
     setResults(null);
     if (searchInputRef.current) {
       searchInputRef.current.focus();
+      setActiveIndex(-1);
     }
+    console.log('Clicked result', query);
   };
 
   const handleBlur = () => {
@@ -80,19 +136,50 @@ function Search({data, keys}: SearchProps): JSX.Element {
 
   return (
     <div>
+      <div role="status" aria-atomic="true" aria-live="polite">
+        <p>{announcement}</p>
+      </div>
       <Input
         ref={searchInputRef}
-        id="search-input"
+        id="autocomplete-input"
         value={query}
         search
         onChange={handleInputChange}
-        // onFocus={() => setIsFocused(true)}
-        // onKeyDown={handleKeyDown}
-        // onBlur={handleBlur}
+        onFocus={() => setIsFocused(true)}
+        onKeyDown={handleKeyDown}
+        onBlur={handleBlur}
+        autoComplete="off"
+        name="input-autocomplete"
+        placeholder="Search something"
+        role="combobox"
+        aria-expanded={results ? true : false}
+        aria-owns="autocomplete-listbox"
         aria-autocomplete="list"
-        aria-controls="autocomplete-list"
         aria-activedescendant={activeIndex >= 0 ? `result-${activeIndex}` : undefined}
+        aria-describedby="autocomplete-hint"
       ></Input>
+      <Fragment>
+        {results && (
+          <ul id="autocomplete-listbox" role="listbox" aria-labelledby="autocomplete-input">
+            {results.map((result, index) => (
+              <li
+                key={index}
+                id={`result-${index}`}
+                role="option"
+                className={activeIndex === index ? 'active' : ''}
+                onClick={() => handleResultClick(result)}
+                onMouseDown={(e) => e.preventDefault()}
+                aria-selected={activeIndex === index}
+                aria-posinset={index}
+                aria-setsize={results.length}
+              >
+                <span>{result.title}</span>
+              </li>
+            ))}
+            {results?.length === 0 && <li>No results found</li>}
+          </ul>
+        )}
+      </Fragment>
       {/* {isFocused && results!.length > 0 && (
         <ul id="autocomplete-list" role="listbox" ref={resultsRef}>
           {results!.map((result, index) => (
