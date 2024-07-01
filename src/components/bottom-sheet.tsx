6bfc69d97d5c2a7de9f081ff883d295a9fabf43a
Alex Vong <avong2011@icloud.com>
2024-07-01 12:34:50 -0400

    Style and added bottom sheet component logic leveraging html dialog element

--- src/components/bottom-sheet.tsx
+++ src/components/bottom-sheet.tsx
@@ -1,15 +1,25 @@
-import {ReactNode, forwardRef, DialogHTMLAttributes, useRef, useImperativeHandle, MouseEvent} from 'react';
+import {
+  ReactNode,
+  forwardRef,
+  DialogHTMLAttributes,
+  useRef,
+  useImperativeHandle,
+  useState,
+  useEffect,
+  MouseEvent,
+} from 'react';
 import './bottom-sheet.scss';
 
 interface DialogProps extends DialogHTMLAttributes<HTMLDialogElement> {
   children: ReactNode;
-  toggleBottomSheet: (e: MouseEvent) => void;
+  toggleBottomSheet: () => void;
 }
 
 const BottomSheet = forwardRef<HTMLDialogElement, DialogProps>(function BottomSheet(
   {children, toggleBottomSheet}: DialogProps,
   ref
 ): JSX.Element {
+  const [isAnimating, setIsAnimating] = useState(false);
   const internalRef = useRef<HTMLDialogElement>(null);
   // Expose the internalDialogRef through the forwarded dialogRef
   useImperativeHandle(ref, () => {
@@ -20,8 +30,56 @@ const BottomSheet = forwardRef<HTMLDialogElement, DialogProps>(function BottomSh
     }
   });
 
+  useEffect(() => {
+    const handleAnimationEnd = (event: AnimationEvent) => {
+      if (internalRef.current) {
+        if (event.animationName === 'close') {
+          // Handle fadeout animation end
+          console.log('Fadeout animation ended');
+          toggleBottomSheet();
+        }
+        internalRef.current.classList.remove('animate-fadeout');
+        // Hide the element after animation ends
+        setIsAnimating(false);
+      }
+    };
+
+    const element = internalRef.current;
+    if (element) {
+      element.addEventListener('animationend', handleAnimationEnd);
+    }
+
+    return () => {
+      if (element) {
+        element.removeEventListener('animationend', handleAnimationEnd);
+      }
+    };
+  }, [isAnimating]);
+
+  const handleClick = (event: MouseEvent) => {
+    if (!isAnimating && internalRef.current) {
+      if (event.currentTarget === event.target) {
+        internalRef.current.classList.add('animate-fadeout');
+        setIsAnimating(true);
+      }
+    }
+  };
+
+  const handleKeydown = (e: React.KeyboardEvent<HTMLDialogElement>) => {
+    if (!internalRef.current) return;
+    switch (e.key) {
+      case 'Escape':
+        e.preventDefault();
+        internalRef.current.classList.add('animate-fadeout');
+        setIsAnimating(true);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <dialog ref={internalRef} className="bottom-sheet" onClick={toggleBottomSheet}>
+    <dialog ref={internalRef} className="bottom-sheet" onKeyDown={handleKeydown} onClick={handleClick}>
       <div className="bottom-sheet__content">{children}</div>
     </dialog>
   );
