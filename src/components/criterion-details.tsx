4522c0453547903b44f4f09ff8af4ce65a9c503b
Alex Vong <avong2011@icloud.com>
2024-07-08 08:31:02 -0400

    Added exceptions logic to capture criterion exceptions or no exceptions

--- src/components/criterion-details.tsx
+++ src/components/criterion-details.tsx
@@ -5,27 +5,50 @@ interface CriterionDetailsProps {
   data: Successcriterion;
 }
 
+enum SpecialCases {
+  EXCEPTION = 'exception',
+  ALLTRUE = 'all_true',
+}
+
 function CriterionDetails({data}: CriterionDetailsProps): JSX.Element {
   const {ref_id, title, description, url, level, brief, special_cases, notes, references} = data;
+
+  const renderBrief = brief?.map((item, index) => {
+    const {title, description} = item;
+    return (
+      <div key={index} className="brief">
+        <div className="brief-title">{title}</div>
+        <div className="brief-description">{description}</div>
+      </div>
+    );
+  });
+
+  const renderSpecialCases = special_cases?.map((item, index) => {
+    const {type, title} = item;
+    return <div key={index}>{title}</div>;
+  });
+
   return (
     <div className="criterion">
       <div className="criterion__header">
         <div className="criterion-title-group">
-          <div className="criterion-overline">Success criteria {ref_id}</div>
+          <div className="criterion-overline">Understanding {ref_id}</div>
           <div className="criterion-title">{title}</div>
           <div className="level-tag">Level {level}</div>
         </div>
-        <div className="criterion-description">{description}</div>
-        {brief?.map((item, index) => {
-          const {title, description} = item;
-          return (
-            <div key={index} className="brief">
-              <div className="brief-title">{title}</div>
-              <div className="brief-description">{description}</div>
-            </div>
-          );
-        })}
+        {renderBrief}
+        <div className="criterion-success">
+          <div className="criterion-subtitle">Success criterion</div>
+          <div className="criterion-description">{description}</div>
+        </div>
       </div>
+      {special_cases && (
+        <div className="special-cases">
+          <div className="special-cases-header">
+            {special_cases[0].type === SpecialCases.EXCEPTION ? 'Exceptions' : 'No exceptions'}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
